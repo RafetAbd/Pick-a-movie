42c1bf6e243380f9e45e5602cb9abd06bdbc05a6
Rafet Abdalgalil <rafetalani@gmail.com>
2022-01-22 18:04:53 -0700

    try to deploy to heroku

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -4,6 +4,8 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const axios = require('axios');
 
+// const privateKey = '12345';
+
 const SALT_ROUNDS = 5;
 
 const User = db.define('user', {
